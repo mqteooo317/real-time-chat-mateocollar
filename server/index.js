66486d529d74b374e6d4780d757265cfef11c9ea
mqteooo317 <collarmateo0610@gmail.com>
2026-04-21 17:29:29 -0300

    Commented critical websockets-related lines

--- server/index.js
+++ server/index.js
@@ -7,22 +7,28 @@ import { createServer } from 'node:http'
 const PORT = process.env.PORT ?? 3000
 const app = express()
 const server = createServer(app)
+// Init Socket.io server on top of HTTP server
 const io = new Server(server, {
+    // Recover messages if user reconnects within 3s
     connectionStateRecovery: {
-        maxDisconnectDuration: 3000 //for avoiding losing messages in an interval of time
+        maxDisconnectDuration: 3000
     }
 })
 
 app.use(logger('dev'))
 
+// Triggers when a client connects via WebSocket
 io.on('connection', function(socket) {
     console.info("an user has connected!")
     
+    // Triggers when that client disconnects
     socket.on('disconnect', function() {
         console.log('an user has disconnected')
     })
     
+    // Listens for 'chat message' event from this client
     socket.on('chat message', function(msg) {
+        // Broadcasts the message to all connected clients
         io.emit('chat message', msg)
     })
 })
